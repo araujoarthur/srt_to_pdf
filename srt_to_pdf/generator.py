b80c20f944ab4ba52337b2a70d298b45c66298c2
Arthur Galdino <arthur.galdino.code@proton.me>
2025-01-19 15:32:18 -0300

    small adjustments on CSS and HTML base template

--- srt_to_pdf/generator.py
+++ srt_to_pdf/generator.py
@@ -6,7 +6,7 @@ HTML_OUTPUT_PATH = FILE_PATH / 'OUTPUT' / 'HTML'
 TEMPLATES_PATH = FILE_PATH / 'templates'
 TEMPLATES_STYLE_PATH = TEMPLATES_PATH / 'styles'
 
-def generate_html(template: str, ctx: dict) -> None:
+def generate_html(template: str, ctx: dict, output_path: str = HTML_OUTPUT_PATH) -> None:
    """
       Generates the HTML that will be used to construct the PDF.
       It takes a template name that must exist within the templates folder and a context dictionary that matches
@@ -15,12 +15,16 @@ def generate_html(template: str, ctx: dict) -> None:
    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_PATH))
    template_render = env.get_template(template + '.html')
    output = template_render.render(media=ctx, css = TEMPLATES_STYLE_PATH / (template + '.css'))
-   ensure_dir(HTML_OUTPUT_PATH)
-   with open(HTML_OUTPUT_PATH / 'output.html', 'w') as f:
+   ensure_dir(output_path)
+   with open(output_path / 'output.html', 'w') as f:
       f.write(output)
 
-def
+def generate_pdf(html_file: str, output_file: str) -> None:
+   pass
 
 def ensure_dir(path: Path) -> None:
+   """
+      Ensures a directory exists.
+   """
    if not path.exists():
        path.mkdir(parents=True)
